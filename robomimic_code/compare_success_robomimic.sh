14495c2ff4e94c4e355199e7f67f3236cdb67f38
rn-1 <rnene@usc.edu>
2026-05-07 01:02:47 -0700

    jgj

--- robomimic_code/compare_success_robomimic.sh
+++ robomimic_code/compare_success_robomimic.sh
@@ -59,7 +59,9 @@ apptainer exec --nv --fakeroot --writable-tmpfs --bind /apps:/apps /scratch1/rne
   export TRANSFORMERS_OFFLINE=1
   mkdir -p \"\$HUGGINGFACE_HUB_CACHE\" \"\$TRANSFORMERS_CACHE\"
   pip install -q -U 'cython<3'
-  pip install -e /project2/jessetho_1732/rl_eval_wm/robometer
+  ( cd /project2/jessetho_1732/rl_eval_wm/robometer && pip install -e . ) \
+    || ( cd /project2/jessetho_1732/rl_eval_wm/robometer && pip install . ) \
+    || export PYTHONPATH=/project2/jessetho_1732/rl_eval_wm/robometer:\${PYTHONPATH:-}
   sed -i 's/field(init=False, metadata=/field(init=False, default=None, metadata=/g' \
     /opt/micromamba/envs/app/lib/python3.12/site-packages/lerobot/policies/groot/groot_n1.py
   cd /project2/jessetho_1732/rl_eval_wm/dino_wm
